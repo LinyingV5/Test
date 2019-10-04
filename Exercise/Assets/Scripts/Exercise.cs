abee0b474b68af528b98244814597aa1326941ed
daguazi <daguazi@126.com>
2019-10-05 00:26:15 +0800

    Welcom！！

---
+++ Exercise/Assets/Scripts/Exercise.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercise : MonoBehaviour
+{
+    void Start()
+    {
+        /*
+         * 在控制台里写下“任意一句话 ——你的名字"
+         */
+        print("请开始你的表演 ——大瓜子");
+    }
+}
